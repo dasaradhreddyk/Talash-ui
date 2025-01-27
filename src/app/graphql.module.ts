7df603ffed0fc6417302ad48b3eaf9497584b979
dasaradhreddyk <58421595+dasaradhreddyk@users.noreply.github.com>
2025-01-27 21:14:31 +1100

    Update graphql.module.ts

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'https://groupdiscussion-aebnhpc7fdhrbcc2.australiasoutheast-01.azurewebsites.net/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://talashbff-a4d6dhaab7c5gnah.australiasoutheast-01.azurewebsites.net/graphql'; // <-- add the URL of the GraphQL server her
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
